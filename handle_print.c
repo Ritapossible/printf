fe6e500194b5284d0258718cfb328c62b347865e
Ritapossible <egwuaturita95@gmail.com>
2023-08-20 03:23:39 -0700

    A program to handle print

--- handle_print.c
+++ handle_print.c
@@ -14,7 +14,7 @@
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	int i, unknown_len = 0, printed_chars = -1;
+	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
@@ -30,9 +30,9 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	{
 		if (fmt[*ind] == '\0')
 			return (-1);
-		unknown_len += write(1, "%%", 1);
+		unknow_len += write(1, "%%", 1);
 		if (fmt[*ind - 1] == ' ')
-			unknown_len += write(1, " ", 1);
+			unknow_len += write(1, " ", 1);
 		else if (width)
 		{
 			--(*ind);
@@ -42,8 +42,8 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 				--(*ind);
 			return (1);
 		}
-		unknown_len += write(1, &fmt[*ind], 1);
-		return (unknown_len);
+		unknow_len += write(1, &fmt[*ind], 1);
+		return (unknow_len);
 	}
 	return (printed_chars);
 }
